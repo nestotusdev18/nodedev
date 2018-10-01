b1e27507cd812d7162d3aa3b2a86458711228392
hari-pro <hari11504anitha@gmail.com>
2018-10-01 18:28:13 +0530

    app.js updated

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/products', product);
 
-let port = 3000;
+let port = 4000;
 
 app.listen(port, () => {
     console.log('Server is up and running on port numner ' + port);
